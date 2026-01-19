f24a5fbfd82b06fba662e5a5371bde71e5662526
Animesh Singh <animeshsingh332@gmail.com>
2026-01-19 12:07:01 +0530

    Add Comparable folder

---
+++ module-1/Comparable/Fundamentals.java
@@ -0,0 +1,15 @@
+/*
+Comparator: 
+	It is an interface present inside java.util package
+	It was introduced in JDK 1.2
+	It is used to define custom sorting order
+	It provide two methods:
+			compare(Object obj1, Object obj2) 
+			equals(Object obj)
+	Sorting logic is written in a separate class
+	It allows multiple sorting sequence
+	It is used when class source code cannot be modified
+	It does not affect the original class structure
+	It is mainly used with collections.sort(), TreeSet, TreeMap 
+	Useful for sorting based on multiple fields
+*/
