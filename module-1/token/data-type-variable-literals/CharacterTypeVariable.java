6790a151ed03cde007ff3e0ceb5f669f216af382
Animesh Singh <animeshsingh332@gmail.com>
2026-02-12 12:09:09 +0530

    Initial commit - Advance Java Project

--- module-1/token/data-type-variable-literals/CharacterTypeVariable.java
+++
@@ -1,7 +0,0 @@
-class CharacterTypeVariable {
-	public static void main(String[] args) {
-		char chartypevariable;
-		chartypevariable = 'a';
-		System.out.println(chartypevariable);
-	}
-}
--- module-1/token/data-type-variable-literals/StringTypeVariable.java
+++
@@ -1,7 +0,0 @@
-class StringTypeVariable{
-	public static void main(String[] args) {
-		String stringTypeVariable;
-		stringTypeVariable = "Java@123";
-		System.out.println(stringTypeVariable);
-	}
-}
